90a3d8cc98639bd1bc0ca1c644a95eb5a0b8510f
Iago Real <iagovreal@gmail.com>
2024-06-07 16:55:10 -0300

    fix tratment error

--- dist/main.js
+++ dist/main.js
@@ -34,17 +34,17 @@ class Calculator {
         return a * b;
     }
     division(a, b) {
-        if (a || b > 1) {
-            return console.log("invalid operation");
+        if (a == 0 || b == 0) {
+            throw new Error("Invalid operation");
         }
         return a / b;
     }
 }
 const calculator = new Calculator();
 function askOperation() {
-    const choseOperation = readline_sync_1.default.question('Chose a mat operation');
+    const choseOperation = readline_sync_1.default.question(`Choose a math operation (Ex: +, -, x or /): `);
     if (!isOperation(choseOperation)) {
-        throw new Error("Invalid Operation");
+        return null;
     }
     return choseOperation;
 }
@@ -52,10 +52,10 @@ function isOperation(operation) {
     return Object.values(Operation).includes(operation);
 }
 function askNumber(orderNumber) {
-    const input = readline_sync_1.default.question(`Enter number ${orderNumber}: `);
+    const input = readline_sync_1.default.question(`Enter the ${orderNumber}º term: `);
     const number = parseFloat(input);
     if (isNaN(number)) {
-        throw new Error("Please, enter a valid number.");
+        return null;
     }
     return number;
 }
@@ -67,13 +67,28 @@ try {
     let continueLoop = true;
     while (continueLoop) {
         const operation = askOperation();
+        if (operation === null) {
+            console.log("Invalid operation. Please try again.");
+            continue;
+        }
         const number1 = askNumber('1');
         const number2 = askNumber('2');
-        const result = calculator.equals(operation, number1, number2);
-        console.log(`The result of ${operation} operation is: ${result}`);
+        if (number1 === null || number2 === null) {
+            console.log("Invalid number. Please try again.");
+            continue;
+        }
+        try {
+            const result = calculator.equals(operation, number1, number2);
+            console.log(`The result of ${operation} operation is: ${result}`);
+        }
+        catch (error) {
+            console.log("not exist division by 0");
+        }
         continueLoop = askContinue();
+        console.clear();
     }
     console.log("Closing program");
+    console.clear();
 }
 catch (error) {
     console.error(error);
--- source/main.ts
+++ source/main.ts
@@ -1,5 +1,4 @@
-import readlineSync from "readline-sync"
-
+import readlineSync from "readline-sync";
 
 enum Operation {
     plus = '+',
@@ -8,8 +7,7 @@ enum Operation {
     divided = "/"
 }
 
-
-class Calculator{ 
+class Calculator { 
     equals(operation: Operation, a: number, b: number): any {
         switch (operation) {
             case Operation.plus:
@@ -17,53 +15,53 @@ class Calculator{
             case Operation.minus:
                 return this.subtraction(a, b);
             case Operation.multiply:
-                    return this.multiplication(a, b);
+                return this.multiplication(a, b);
             case Operation.divided:
-                    return this.division(a, b);        
+                return this.division(a, b);        
         }
     }
     
-    private addition (a: number, b:number): number{
+    private addition(a: number, b: number): number {
         return a + b;
     }
     
-    private subtraction (a: number, b:number): number{
+    private subtraction(a: number, b: number): number {
         return a - b;
     }
 
-    private multiplication (a: number, b:number): number{
+    private multiplication(a: number, b: number): number {
         return a * b;
     }
 
-    private division (a: number, b:number): any{
-        if (a || b > 1){
-            return console.log("invalid operation");
-            }
+    private division(a: number, b: number): any {
+        if (a == 0 || b == 0) {
+            throw new Error("Invalid operation");
+        }
         return a / b;
     }
 }
 
-const calculator = new Calculator();
+const calculator = new Calculator();    
 
-function askOperation(): Operation{
-    const choseOperation = readlineSync.question('Chose a mat operation')
+function askOperation(): Operation | null {
+    const choseOperation = readlineSync.question(`Choose a math operation (Ex: +, -, x or /): `);
     
     if (!isOperation(choseOperation)) {
-        throw new Error("Invalid Operation");
+        return null;
     }
     return choseOperation as Operation;
 }
 
 function isOperation(operation: any): operation is Operation {
     return Object.values(Operation).includes(operation);
-  }
+}
 
-function askNumber(orderNumber:string): number {
-    const input = readlineSync.question(`Enter number ${orderNumber}: `);
+function askNumber(orderNumber: string): number | null{
+    const input = readlineSync.question(`Enter the ${orderNumber}º term: `);
     const number = parseFloat(input);
 
     if (isNaN(number)) {
-        throw new Error("Please, enter a valid number.");    
+        return null
     }
     return number;
 }
@@ -74,22 +72,33 @@ function askContinue(): boolean {
 }
 
 try {
+    let continueLoop = true;
+    while (continueLoop) {
+        const operation = askOperation();
+        if (operation === null) {
+            console.log("Invalid operation. Please try again.");
+            continue;
+        }
+        const number1 = askNumber('1');
+        const number2 = askNumber('2');
 
-let continueLoop = true
-while (continueLoop) {
-    const operation = askOperation();   
-    const number1 = askNumber('1')
-    const number2 = askNumber('2')
+        if (number1 === null || number2 === null) {
+            console.log("Invalid number. Please try again.");
+            continue;
+        }
 
-    const result = calculator.equals(operation, number1, number2);
-    console.log(`The result of ${operation} operation is: ${result}`);
+        try {
+            const result = calculator.equals(operation, number1, number2);
+            console.log(`The result of ${operation} operation is: ${result}`);
+        } catch (error) {
+            console.log("not exist division by 0");
+        }
 
-    continueLoop = askContinue();
-
+        continueLoop = askContinue();
+        console.clear();
     }
     console.log("Closing program");
-     
-}
-    catch (error) {
-        console.error(error)
+    console.clear(); 
+} catch (error) {
+    console.error(error);
 }
